40538ed834a2f2c21f3d18106b650ba7cf61fe40
岳晨飞 <yuechenfei@boswinner.com>
2023-04-02 10:23:41 +0800

    backup .config

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,47 @@
+local opt = vim.opt
+
+-- 行号
+opt.relativenumber = true
+opt.number = true
+
+-- 缩进
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- 防止包裹
+opt.wrap = false
+
+-- 光标行
+opt.cursorline = true
+
+-- 启用鼠标
+opt.mouse:append("a")
+
+-- 系统剪贴板
+opt.clipboard:append("unnamedplus")
+
+-- 默认新窗口右和下
+opt.splitright = true
+opt.splitbelow = true
+
+-- 搜索
+opt.ignorecase = true
+opt.smartcase = true
+
+-- 外观
+opt.cmdheight = 1
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.pumblend = 5
+opt.background = 'dark'
+opt.winblend = 0
+opt.cursorline = true
+vim.cmd [[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]]
+-- vim.cmd[[colorscheme tokyonight-moon]]
