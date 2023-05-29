61cb65cbbe3f29d1e708ec5557f54a20e002230f
岳晨飞 <yuechenfei@boswinner.com>
2023-05-29 22:46:59 +0800

    fix

--- .config/nvim/lua/yuechenfei/plugins.lua
+++ .config/nvim/lua/yuechenfei/plugins.lua
@@ -1,9 +1,9 @@
 -- 自动安装packer
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -39,43 +39,43 @@ return require('packer').startup(function(use)
   use 'nvim-lualine/lualine.nvim' -- Statusline
 
   -- 用ctrl-hjkl来定位窗口
-  use "christoomey/vim-tmux-navigator" 
+  use "christoomey/vim-tmux-navigator"
 
   -- 文档树
   use {
-    'nvim-tree/nvim-tree.lua',  -- 文档树
+    'nvim-tree/nvim-tree.lua',       -- 文档树
     requires = {
       'nvim-tree/nvim-web-devicons', -- 文档树图标
     }
   }
 
   use "nvim-treesitter/nvim-treesitter" -- 语法高亮
-  use "p00f/nvim-ts-rainbow" -- 配合treesitter，不同括号颜色区分
+  use "p00f/nvim-ts-rainbow"            -- 配合treesitter，不同括号颜色区分
 
   use {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",  -- 这个相当于mason.nvim和lspconfig的桥梁
+    "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
     "neovim/nvim-lspconfig"
   }
-  
+
   -- 自动补全
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-nvim-lsp"
   use "L3MON4D3/LuaSnip" -- snippets引擎，不装这个自动补全会出问题
   use "saadparwaiz1/cmp_luasnip"
   use "rafamadriz/friendly-snippets"
-  use "hrsh7th/cmp-path" -- 文件路径
+  use "hrsh7th/cmp-path"        -- 文件路径
 
-  use "numToStr/Comment.nvim" -- gcc和gc注释
-  use "windwp/nvim-autopairs" -- 自动补全括号
+  use "numToStr/Comment.nvim"   -- gcc和gc注释
+  use "windwp/nvim-autopairs"   -- 自动补全括号
 
   use "akinsho/bufferline.nvim" -- buffer分割线
   use "lewis6991/gitsigns.nvim" -- 左则git提示
 
   use 'nvim-telescope/telescope-file-browser.nvim'
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
-    requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.1', -- 文件检索
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   use "windwp/nvim-ts-autotag"
@@ -84,11 +84,11 @@ return require('packer').startup(function(use)
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
   })
-  use "glepnir/lspsaga.nvim" -- LSP UIs
-  use "onsails/lspkind-nvim" -- vscode-like pictograms
+  use "glepnir/lspsaga.nvim"            -- LSP UIs
+  use "onsails/lspkind-nvim"            -- vscode-like pictograms
   use "jose-elias-alvarez/null-ls.nvim" -- Use Neovim as  a language server to inject LSP diagnostics, code actions, and more via Lua
 
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use 'dinhhuy258/git.nvim'             -- For git blame & browse
   if packer_bootstrap then
     require('packer').sync()
   end
