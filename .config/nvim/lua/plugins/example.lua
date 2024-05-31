a2e78f2168b600ac660ba3200b5be99ba1b49e27
john-yue <ooo.c.f.yue.ooo@gmail.com>
2024-05-31 14:43:25 +0800

    init：重新配置dotConfig

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,6 +1,9 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  -- amongst your other plugins
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
