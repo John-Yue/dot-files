2d1f9ab7182aeb6241be4f8b4affd042360cd7cc
john-yue <ooo.c.f.yue.ooo@gmail.com>
2024-10-14 22:39:14 +0800

    feat: add clear screen command
    fish shell 没有清屏快捷键，这里写了一个清屏方法

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,14 @@ set -gx EDITOR nvim
 set --universal nvm_default_version v16.20.2
 set --universal nvm_defualt_package yarn np
 
+# bind ctrl+/ 清屏命令
+bind \x1f clear_screen
+
+function clear_screen
+    clear
+    commandline -f repaint
+end
+
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
